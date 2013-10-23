0ecfb7d1d93298e4a198035b11f29f1cf1b377a2
Jesus Salcedo <jsalcedo@jsalcedo.(none)>
2013-10-23 12:09:54 -0430

    keep 2

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -18,67 +18,79 @@ import play.libs.F.*;
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
-
 /**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+ * 
+ * Simple (JUnit) tests that can call all parts of a play app. If you are
+ * interested in mocking a whole application, see the wiki for more details.
+ * 
+ */
 public class ApplicationTest {
 
-    @Test 
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
-    
-    
-    @Test 
-    public void mutantCheck()throws Exception {
-    	String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"}; 
-    	/**
-    	 * 
-    	 * A T G C G A
-    	 * C A G T G C
-    	 * T T A T G T
-    	 * A G A A G G
-    	 * C C C C T A
-    	 * T C A C T G
-    	 * 
-    	 * 
-    	 * 
-    	 * ***/
-    	
-        assertThat(isMutant(dna)).isEqualTo(true);
-    }
+	@Test
+	public void simpleCheck() {
+		int a = 1 + 1;
+		assertThat(a).isEqualTo(2);
+	}
 
-    
-    private boolean isMutant(String[] dna) throws Exception{
-    	int count = 0;
-    	int total = 0;
-    	int hor = 0;
-    	for(int i = 0; i<dna.length;i++){
-    		char pre = dna[i].charAt(0);
-    		for(int y=0;y<dna.length;y++){
-    			final char curr = dna[i].charAt(y);
-    				if(pre == curr){
-    					count++;
-    					pre = curr;
-    				}else if(count>=3){
-    					total++;
-    					pre = curr;
-    					count = 0;
-    				}else{
-    					count = 0;
-    					pre = curr;
-    				}    	
-    		System.out.print("["+dna[i].charAt(y)+"]");
-    		}    		
-    		System.out.println("");
-    	}
-    	return 	true;
-    }
+	@Test
+	public void mutantCheck() throws Exception {
+		String[] dna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA",
+				"TCACTG" };
+		/**
+		 * 
+		 * A T G C G A 
+		 * C A G T G C 
+		 * T T A T G T 
+		 * A G A A G G
+		 * C C C C T A
+		 * T C A C T G
+		 * 
+		 * 
+		 * 
+		 * ***/
 
-   
+		assertThat(isMutant(dna)).isEqualTo(true);
+	}
+
+	private boolean isMutant(String[] dna) throws Exception {
+		int count = 0;
+		int total = 0;
+		int hor = 0;
+		for (int i = 0; i < dna.length; i++) {
+			char pre = dna[i].charAt(0);
+			for (int y = 0; y < dna.length; y++) {
+
+				final char curr = dna[i].charAt(y);
+				if (checkMutantHelper(dna, i, y) >= 3) {
+					total++;
+				} else if (pre == curr) {
+					count++;
+					pre = curr;
+				} else if (count >= 3) {
+					total++;
+					pre = curr;
+					count = 0;
+				} else {
+					count = 0;
+					pre = curr;
+				}
+
+			}
+		}
+		System.out.println("total ="+total);
+		return true;
+	}
+
+	private int checkMutantHelper(String[] dna, int r, int c) {
+		if (dna.length - (r+1) > 0 && dna.length - (c+1) > 0) {
+			if (dna[r].charAt(c) == dna[r + 1].charAt(c + 1)) {
+				return 1 + checkMutantHelper(dna, r + 1, c + 1);
+			} else {
+				return checkMutantHelper(dna, r + 1, c + 1);
+			}
+		} else {
+			return 0;
+		}
+	}
+
 }
